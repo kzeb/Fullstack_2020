b4be1a3645c0b9f7eed7721f9f629783852376ed
Karol Żebrowski <19karol98@gmail.com>
2020-04-24 10:40:41 +0200

    unicafe step2, step3, step4

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,11 +1,25 @@
 import React from "react";
 
-const Statistics = (props) => {
+const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+  let all = good + neutral + bad;
+  let average = (good * 1 + neutral * 0 + bad * -1) / all;
+  let positive = (good * 100) / all;
+
   return (
     <>
-      <p>good: {props.good}</p>
-      <p>neutral: {props.neutral}</p>
-      <p>bad: {props.bad}</p>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive} %</p>
     </>
   );
 };
