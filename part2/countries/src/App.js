b40655e8e7f005979f4e1add521cb16c029ebf0d
Karol <19karol98@gmail.com>
2022-12-04 21:26:06 +0100

    2_14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const App = () => {
   const [result, setResult] = useState([]);
+  const [weather, setWeather] = useState({});
 
   const handleCountryNameChange = async (event) => {
     if (event.target.value.length > 0) {
@@ -10,6 +11,26 @@ const App = () => {
         .get(`https://restcountries.com/v3.1/name/${event.target.value}`)
         .then((response) => {
           setResult(response.data);
+          if (response.data.length === 1) {
+            axios
+              .get(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${response.data[0].capitalInfo.latlng[0]}&lon=${response.data[0].capitalInfo.latlng[1]}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+              )
+              .then((response) => {
+                setWeather({
+                  temperature: response.data.main.temp,
+                  wind: response.data.wind.speed,
+                  icon: response.data.weather[0].icon,
+                });
+              })
+              .catch((err) => {
+                setWeather({
+                  temperature: "response.main.temp",
+                  wind: "response.wind.speed",
+                  icon: "response.weather[0].icon",
+                });
+              });
+          }
         })
         .catch((err) => {
           setResult([]);
@@ -19,8 +40,26 @@ const App = () => {
     }
   };
 
-  const handleButtonClick = (element) => {
+  const handleButtonClick = async (element) => {
     setResult([element]);
+    await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${element.capitalInfo.latlng[0]}&lon=${element.capitalInfo.latlng[1]}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+      )
+      .then((response) => {
+        setWeather({
+          temperature: response.data.main.temp,
+          wind: response.data.wind.speed,
+          icon: response.data.weather[0].icon,
+        });
+      })
+      .catch((err) => {
+        setWeather({
+          temperature: "response.main.temp",
+          wind: "response.wind.speed",
+          icon: "response.weather[0].icon",
+        });
+      });
   };
 
   return (
@@ -32,13 +71,20 @@ const App = () => {
             <h1>{result[0].name.common}</h1>
             <p>capital {result[0].capital}</p>
             <p>area {result[0].area}</p>
-            <h2>languages:</h2>
+            <h3>languages:</h3>
             <ul>
               {Object.entries(result[0].languages).map((lang) => (
                 <li key={lang[1]}>{lang[1]}</li>
               ))}
             </ul>
             <img src={result[0].flags.png} alt="FLAG" />
+            <h2>Weather in {result[0].capital}</h2>
+            <p>temperature {weather.temperature} Celsius</p>
+            <img
+              src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+              alt="WEATHER_ICON"
+            />
+            <p>wind {weather.wind} m/s</p>
           </>
         ) : (
           result.map((element) => (
